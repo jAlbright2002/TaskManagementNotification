a9c73478a8aecf3b831a5a56ed4bc1b624908be8
james <G00379271@atu.ie>
2024-11-10 14:49:42 +0000

    Project created

---
+++ src/main/java/ie/atu/taskmanangementnotification/TaskManangementNotificationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.taskmanangementnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManangementNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManangementNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/taskmanangementnotification/TaskManangementNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.taskmanangementnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManangementNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
