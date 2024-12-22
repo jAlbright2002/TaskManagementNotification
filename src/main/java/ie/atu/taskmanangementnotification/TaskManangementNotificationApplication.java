e5405b1ac131981cde3a707508d22da9a4108c6e
james <G00379271@atu.ie>
2024-12-22 17:30:43 +0000

    Trying something

--- src/main/java/ie/atu/taskmanangementnotification/TaskManangementNotificationApplication.java
+++ src/main/java/ie/atu/taskmanangementnotification/TaskManangementNotificationApplication.java
@@ -1,9 +1,11 @@
 package ie.atu.taskmanangementnotification;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class TaskManangementNotificationApplication {
 
     public static void main(String[] args) {
